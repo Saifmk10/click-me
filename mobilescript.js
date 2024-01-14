bde13b7b2ce09a79721fd5a025076aae0c2d4381
Saifmk10 <150276205+Saifmk10@users.noreply.github.com>
2024-01-14 12:42:00 +0530

    Update mobilescript.js

--- mobilescript.js
+++ mobilescript.js
@@ -1,16 +1,14 @@
-// document.getElementById("counter").innerText = 3;
-
 let counting = document.getElementById("counter");
+let data = document.getElementById("savedData");
 let count = 0;
-let resetData = 0;
 
 function increment(){
     count = count + 1;
     counting.innerText = count;
 }
 
-function reset(){
+function savedata(){
+    data.innerText +=count + " - ";
     count = 0;
-    counting.innerHTML = count;
+    counting.innerText = 0;
 }
-
